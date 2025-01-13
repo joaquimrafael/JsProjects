844526888f893f611cfe3f4924817c87a6f8d13c
Joaquim Prieto <joaquim.mpp@outlook.com>
2025-01-13 16:24:55 -0300

    operadores

---
+++ operadores/js/script.js
@@ -0,0 +1,43 @@
+// 1 Numbers
+
+// var x = 10;
+// console.log(typeof(x));
+// console.log(typeof(6.12));
+// console.log(typeof(6.12e5));
+// console.log(typeof(-121.1));
+
+// 2 Operadores
+
+// console.log(9 + (10 * 2));
+// console.log(9 / 2);
+
+// 3 Special Numbers
+
+// console.log(1 / 0);
+// console.log(typeof (Infinity));
+// console.log(typeof (-Infinity));
+// console.log(12 * "a");
+
+//  4 Strings
+
+// console.log("Hello World");
+// console.log('Hello World'); 
+// console.log(typeof ("Hello World")); 
+// console.log(typeof ('Hello World'));
+// console.log("Ola mundo".length);
+
+// 5 Special Characters
+
+// console.log("Hello World \n How are you?");
+// console.log("Hello World \t How are you?");
+
+
+// 6 Concatenação
+
+console.log("Hello" + " World");
+console.log('testando' + ' a' + ' concatenação');
+
+// 7 Interpolação
+
+var name = "John";
+console.log(`Hello ${name}`);
